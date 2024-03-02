1c73c52736da7618b8f439b54176528610d2439b
martinromario55 <martinromario55@gmail.com>
2024-03-02 08:50:10 +0300

    User Profile Screen

--- src/screens/UserAccountScreen.tsx
+++ src/screens/UserAccountScreen.tsx
@@ -1,14 +1,84 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import * as React from 'react';
+import {Text, View, StyleSheet, StatusBar, Image} from 'react-native';
+import {COLORS, FONTFAMILY, FONTSIZE, SPACING} from '../theme/theme';
+import AppHeader from '../components/AppHeader';
+import SettingsComponent from '../components/SettingsComponent';
 
-const UserAccountScreen = () => {
+const UserAccountScreen = ({navigation}: any) => {
   return (
-    <View>
-      <Text>UserAccountScreen</Text>
+    <View style={styles.container}>
+      <StatusBar hidden />
+      <View style={styles.appHeaderContainer}>
+        <AppHeader
+          name="close"
+          header={'My Profile'}
+          action={() => navigation.goBack()}
+        />
+      </View>
+
+      <View style={styles.profileContainer}>
+        <Image
+          source={require('../assets/image/avatar.png')}
+          style={styles.avatarImage}
+        />
+        <Text style={styles.avatarText}>John Doe</Text>
+      </View>
+
+      <View style={styles.profileContainer}>
+        <SettingsComponent
+          icon="user"
+          heading="Account"
+          subheading="Edit Profile"
+          subtitle="Change Password"
+        />
+        <SettingsComponent
+          icon="setting"
+          heading="Settings"
+          subheading="Theme"
+          subtitle="Permissions"
+        />
+        <SettingsComponent
+          icon="dollar"
+          heading="Offers & Refferrals"
+          subheading="Offer"
+          subtitle="Refferrals"
+        />
+        <SettingsComponent
+          icon="info"
+          heading="About"
+          subheading="About Movies"
+          subtitle="more"
+        />
+      </View>
     </View>
   );
 };
 
-export default UserAccountScreen;
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  appHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_20 * 2,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    padding: SPACING.space_36,
+  },
+  avatarImage: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+  },
+  avatarText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_16,
+    marginTop: SPACING.space_16,
+    color: COLORS.White,
+  },
+});
 
-const styles = StyleSheet.create({});
+export default UserAccountScreen;
